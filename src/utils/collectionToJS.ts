535299805c8439cd782184cbe896c4716300f032
Felipe Hefler <hefler@users.noreply.github.com>
2025-01-07 08:29:21 +0100

    feat(collectionToJS): enhance variable processing to support linked variable aliases and improve output formatting

--- src/utils/collectionToJS.ts
+++ src/utils/collectionToJS.ts
@@ -22,15 +22,29 @@ async function processCollection({
           let currentObj = variables[toCamelCase(mode.name)];
           const parts = name.split("/").map(toCamelCase);
 
-          parts.forEach((part, i) => {
+          parts.forEach(async (part, i) => {
             if (i === parts.length - 1) {
-              currentObj[part] = resolvedType === "COLOR" 
-                ? rgbToCssColor(value as RGBA)
-                : resolvedType === "FLOAT"
-                  ? parseFloat(value as string)
-                  : resolvedType === "BOOLEAN"
-                    ? Boolean(value)
-                    : String(value);
+              if (typeof value === 'object' && 'type' in value && value.type === 'VARIABLE_ALIAS') {
+                const linkedVar = await figma.variables.getVariableByIdAsync(value.id);
+                
+                if (linkedVar) {
+                  const linkedVarCollection = await figma.variables.getVariableCollectionByIdAsync(linkedVar.variableCollectionId);
+                  const collPrefix = linkedVarCollection && linkedVarCollection.name !== name ? 
+                    `${toCamelCase(linkedVarCollection.name)}.` : '';
+                  
+                  currentObj[part] = `${collPrefix}${toCamelCase(mode.name)}.${linkedVar.name.split('/').map(toCamelCase).join('.')}`;
+                } else {
+                  currentObj[part] = '_unlinked';
+                }
+              } else {
+                currentObj[part] = resolvedType === "COLOR" 
+                  ? rgbToCssColor(value as RGBA)
+                  : resolvedType === "FLOAT"
+                    ? parseFloat(value as string)
+                    : resolvedType === "BOOLEAN"
+                      ? Boolean(value)
+                      : String(value);
+              }
             } else {
               currentObj[part] = currentObj[part] || {};
               currentObj = currentObj[part];
@@ -43,7 +57,11 @@ async function processCollection({
 
   const varName = toCamelCase(name);
   return `export const ${varName} = ${JSON.stringify(variables, null, 2)
-    .replace(/"([^"]+)":/g, '$1:')};\n`;
+    .replace(/"([^"]+)":/g, '$1:')
+    .replace(/"([$_a-zA-Z][$_a-zA-Z0-9]*(?:\.[$_a-zA-Z][$_a-zA-Z0-9]*)*(?:\.\d+)*(?:\.[$_a-zA-Z][$_a-zA-Z0-9]*)*)"/g, (match, p1) => {
+        return p1.replace(/\.(\d+)(?=\.|$)/g, '["$1"]');
+      })
+      .replace(/([0-9]+):/g, '"$1":')};\n`;
 }
 
 export const exportToJS = async (): Promise<string | undefined> => {
