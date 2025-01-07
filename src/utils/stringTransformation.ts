05fa9600c624e5543d959d33d0f09434d0fd8238
Felipe Hefler <hefler@users.noreply.github.com>
2025-01-07 07:29:49 +0100

    feat: extend string transformation utilities with toCamelCase function

--- src/utils/stringTransformation.ts
+++ src/utils/stringTransformation.ts
@@ -1,8 +1,17 @@
-export const toCssVar = (string: string, prependDoubleDash:boolean=false) => {
+export const toCssVar = (string: string, prependDoubleDash: boolean = false) => {
     string = (prependDoubleDash ? `--${string}` : string)
                 .replace(/\//g, "--")
                 .replace(/\s/g, '-')
                 .replace(/\./g, '_')
                 .toLowerCase();
     return string;
+}
+
+export const toCamelCase = (string: string) => {
+    return string
+        .trim()
+        .replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match, index) => 
+            index === 0 ? match.toLowerCase() : match.toUpperCase()
+        )
+        .replace(/-/g, '_');
 }
