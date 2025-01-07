5e2ce791ccc3b3e421f0c219556375f7bd5a7589
Felipe Hefler <hefler@users.noreply.github.com>
2025-01-07 08:28:27 +0100

    fix(stringTransformation): enhance toCamelCase function to handle whitespace and digits correctly

--- src/utils/stringTransformation.ts
+++ src/utils/stringTransformation.ts
@@ -10,9 +10,12 @@ export const toCssVar = (string: string, prependDoubleDash: boolean = false) => 
 export const toCamelCase = (string: string) => {
     return string
         .trim()
-        .replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match, index) => 
-            index === 0 ? match.toLowerCase() : match.toUpperCase()
-        )
+        .replace(/(?:^\w|[A-Z]|\b\w|\s+\w|\d+)/g, (match, index) => {
+            if (index === 0) return match.toLowerCase();
+            if (/^\s+\w/.test(match)) return match.trim().toUpperCase();
+            if (/^\d+$/.test(match)) return match;
+            return match.toUpperCase();
+        })
         .replace(/-/g, '')
         .replace(/\./g, '_');
 }
